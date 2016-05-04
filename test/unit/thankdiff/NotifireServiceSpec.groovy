d7b2dc3a765c6edeb39aa7ced0ab1ea4649bb687
Chakru <abhishek.syncro@gmail.com>
2016-05-04 14:41:58 +0530

    Added files via upload
    creating project

---
+++ test/unit/thankdiff/NotifireServiceSpec.groovy
@@ -0,0 +1,20 @@
+package thankdiff
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(NotifireService)
+class NotifireServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
